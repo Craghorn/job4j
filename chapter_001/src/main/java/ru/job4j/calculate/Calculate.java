33985f1cd9c1daa4f6281ff6eceb9a6f59d335c8
Craghorn <craghorn@gmail.com>
2019-04-02 14:57:54 +0300

    1.3. JavaDoc. Code convention[#128744]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
-
+/**
+ * Какой то тестовый javadoc для класса
+ * проверка
+ */
 public class Calculate
 {
+	/**
+	 * тестовый javadoc для метода главного
+	 * @param args - args.
+	 * @return Hello world.
+	 */
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
