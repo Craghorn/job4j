d634ab3e33f1f153f59770fb512e582bed8872ed
Craghorn <craghorn@gmail.com>
2019-04-04 14:47:36 +0300

    3.2. Конвертер валюты.[#128730]

---
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,43 @@
+package ru.job4j.converter;
+
+/**
+ * Корвертор валюты.
+ */
+public class Converter {
+
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+        return value/70;
+    }
+
+    /**
+     * Конвертируем евро в рубли.
+     * @param value евро.
+     * @return рубли.
+     */
+    public int EuroToruble(int value) {
+        return value*70;
+    }
+
+    /**
+     * Конвертируем доллары в рубли.
+     * @param value доллары.
+     * @return рубли.
+     */
+    public int DollarToruble(int value) {
+        return value*60;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары
+     */
+    public int rubleToDollar(int value) {
+        return value/60;
+    }
+}
